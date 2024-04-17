8e5b870ada7533e797ce157b6c88e8a9cf910a66
Santiago Bedoya <santiagobedoya@live.com>
2024-04-16 20:51:12 -0500

    Creating missing entities and adjustiong existing ones

---
+++ CommUnity/CommUnity.Shared/Entities/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommUnity.Shared.Entities
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+
+        public string Plate { get; set; } = null!;
+
+        public int ApartmentId { get; set; }
+
+        public Apartment? Apartment { get; set; }
+    }
+}
