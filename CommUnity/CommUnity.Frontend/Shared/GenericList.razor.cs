b048abbea0068e62d74219f87062a8670d794804
Santiago Bedoya <santiagobedoya@live.com>
2024-04-17 19:18:58 -0500

    Creating Shared components in fornt end

---
+++ CommUnity/CommUnity.Frontend/Shared/GenericList.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace CommUnity.FrontEnd.Shared
+{
+    public partial class GenericList<TItem>
+    {
+        [Parameter] public RenderFragment? Loading { get; set; }
+
+        [Parameter] public RenderFragment? NoRecords { get; set; }
+
+        [EditorRequired, Parameter] public RenderFragment? Body { get; set; }
+
+        [EditorRequired, Parameter] public List<TItem>? MyList { get; set; }
+    }
+}
