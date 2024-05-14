aae3d6e891ae1e322bcc88ea9c3a52f3c5abcacd
Luis Miguel Viana Villa <viana2093@outlook.com>
2024-05-13 20:19:23 -0500

    Security BackEnd

---
+++ CommUnity/CommUnity.Shared/Enums/UserType.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace CommUnity.Shared.Enums
+{
+    public enum UserType
+    {
+        [Description("Administrador")]
+        Admin,
+
+        [Description("Aministrador Unidad Residencial")]
+        AdminResidentialUnit,
+
+        [Description("Trabajador")]
+        Worker,
+
+        [Description("Residente")]
+        Resident
+    }
+}
