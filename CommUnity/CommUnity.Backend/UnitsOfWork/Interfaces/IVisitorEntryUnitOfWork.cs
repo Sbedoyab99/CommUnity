9da73db4e74d20de3f4f5b8641730b8f3ee5d755
Santiago Bedoya <santiagobedoya@live.com>
2024-06-06 01:30:28 -0500

    ConfirmVisitorEntry

--- CommUnity/CommUnity.Backend/UnitsOfWork/Interfaces/IVisitorEntryUnitOfWork.cs
+++ CommUnity/CommUnity.Backend/UnitsOfWork/Interfaces/IVisitorEntryUnitOfWork.cs
@@ -1,5 +1,6 @@
 ﻿using CommUnity.Shared.DTOs;
 using CommUnity.Shared.Entities;
+using CommUnity.Shared.Enums;
 using CommUnity.Shared.Responses;
 
 namespace CommUnity.BackEnd.UnitsOfWork.Interfaces
@@ -7,5 +8,9 @@ namespace CommUnity.BackEnd.UnitsOfWork.Interfaces
     public interface IVisitorEntryUnitOfWork
     {
         Task<ActionResponse<VisitorEntry>> ScheduleVisitor(string email, VisitorEntryDTO visitorEntryDTO);
+
+        Task<ActionResponse<IEnumerable<VisitorEntry>>> GetVisitorEntryByStatus(string email, VisitorStatus status);
+
+        Task<ActionResponse<VisitorEntry>> ConfirmVisitorEntry(string email, VisitorEntryDTO visitorEntryDTO);
     }
 }
